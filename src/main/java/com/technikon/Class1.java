c0a4fb292d6b8aae72c410f9ca5add38d08a5d9a
johnbl <ic121012@di.uoa.gr>
2024-08-02 13:25:07 +0300

    hello

---
+++ src/main/java/com/technikon/Class1.java
@@ -0,0 +1,3 @@
+package com.technikon;
+
+public class Class1 { }
---
+++ src/main/java/com/technikon/Main.java
@@ -0,0 +1,18 @@
+package com.technikon;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    public static void method1() {
+        System.out.println("i am 1");
+    }
+
+    public static void method2() {
+        System.out.println("i am 2");
+    }
+}
