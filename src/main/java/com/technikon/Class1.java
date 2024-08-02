6ca0dd4d402a14c314fb1b0090c6de49855aaafc
mpapangelis <mariospap2004@gmail.com>
2024-08-02 14:56:02 +0300

    test

--- src/main/java/com/technikon/Class1.java
+++ src/main/java/com/technikon/Class1.java
@@ -4,4 +4,9 @@ package com.technikon;
 public class Class1 { 
     private int apples;
     public int newintemorellas;
-        }
+    
+    
+    public void hello(){
+        System.out.println("dwaj");
+    }
+}
